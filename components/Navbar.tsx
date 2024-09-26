6ad1c1103b3790dbb5bd13696bf6888eb086dda1
Mary Ang <maryanggithub@bcssti.com>
2024-09-26 16:33:26 +0800

    fix: vercel build error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,6 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
 const components: { title: string; href: string; description: string }[] = [
