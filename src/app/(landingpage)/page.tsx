f625d90b031e0f0fdd34875bab472f5dbc125515
MaryAngGit2024 <maryanggithub@bcssti.com>
2024-09-27 17:43:37 +0800

    fix: vercel build error

--- src/app/(landingpage)/page.tsx
+++ src/app/(landingpage)/page.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "../../../components/Navbar";
+// import { Navbar } from "../../../components/Navbar";
 import About from "./(sections)/about";
 import { HeadersLanding } from "./(sections)/headers";
 import { Posiflex } from "./(sections)/posiflex";
